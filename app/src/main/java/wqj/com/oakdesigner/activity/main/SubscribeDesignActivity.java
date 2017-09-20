054ff708f888cbeda8846841c18556672bccc306
wqjuser <316400644@qq.com>
2017-09-20 18:16:57 +0800

    新增预约界面

--- app/src/main/java/wqj/com/oakdesigner/activity/main/SubscribeDesignActivity.java
+++ app/src/main/java/wqj/com/oakdesigner/activity/main/SubscribeDesignActivity.java
@@ -3,13 +3,28 @@ package wqj.com.oakdesigner.activity.main;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.coorchice.library.SuperTextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import wqj.com.oakdesigner.R;
+import wqj.com.oakdesigner.utils.adjuster.RippleAdjuster;
 
 public class SubscribeDesignActivity extends AppCompatActivity {
+
+    @BindView(R.id.superTitle)
+    SuperTextView mSuperTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscribe_design);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    public void initView() {
+        SuperTextView.Adjuster adjuster = new RippleAdjuster(R.color.opacity_3_white);
+        mSuperTitle.setAdjuster(adjuster);
     }
 }
